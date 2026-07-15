ef7460b2fc2b320152d9b864fe5bca9ec9338978
Msvenkat1990 <msvenkat1990@gmail.com>
2026-07-15 10:41:36 +0530

    Initial commit

---
+++ src/app/app.ts
@@ -0,0 +1,33 @@
+import { Component, signal } from '@angular/core';
+
+import { Footer } from './components/footer/footer';
+import { About } from './components/about/about';
+import { Contact } from './components/contact/contact';
+import { Experience } from './components/experience/experience';
+import { Hero } from './components/hero/hero';
+import { Navbar } from './components/navbar/navbar';
+import { Projects } from './components/projects/projects';
+import { Skills } from './components/skills/skills';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+
+  imports: [
+    About,
+    Contact,
+    Experience,
+    Footer,
+    Hero,
+    Navbar,
+    Projects,
+    Skills
+  ],
+
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+
+export class App {
+  protected readonly title = signal('portfolio-website');
+}
